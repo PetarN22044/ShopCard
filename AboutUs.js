9fc847c605e136da727cdd2797fd6ec1a09080e9
PetarN2204 <adonah223344@gmail.com>
2023-06-19 21:50:02 +0200

    novo za abiut us

---
+++ AboutUs.js
@@ -0,0 +1,5 @@
+function toggleAnswer(index) {
+    let answer = document.getElementById(`answer-${index}`);
+    answer.style.display = answer.style.display === 'none' ? 'block' : 'none';
+}
+// vo ovoj del imame funkcija vo slucaj ako stegnis na prasanjana da izlezit odgovor
