554c878d2e3c08fdf609c62c7b6b9ab4e19b2e6e
PetarN2204 <adonah223344@gmail.com>
2023-06-20 01:31:52 +0200

    vvv

--- AboutUs.js
+++ AboutUs.js
@@ -3,3 +3,18 @@ function toggleAnswer(index) {
     answer.style.display = answer.style.display === 'none' ? 'block' : 'none';
 }
 // vo ovoj del imame funkcija vo slucaj ako stegnis na prasanjana da izlezit odgovor
+
+
+var siteDescription = document.getElementById('siteDescription');
+
+document.addEventListener('mousemove', function(event) {
+  var mouseX = event.clientX;
+
+  if (mouseX < 50) {
+    siteDescription.classList.add('show');
+  } else {
+    siteDescription.classList.remove('show');
+  }
+});
+
+
--- product.js
+++ product.js
@@ -1,41 +1,41 @@
 let products = [
     {
-      name: 'Продукт 1',
-      description: 'Опис на продукт 1',
+      name: 'Product 1',
+      description: '    Lorem ipsum dolor, sit amet consectetur adipisicing',
       image: '../ShopCard/herba Products Img/herba1.png'
     },
     {
-      name: 'Продукт 2',
-      description: 'Опис на продукт 2',
+      name: 'Product 2',
+      description: '    Lorem ipsum dolor, sit amet consectetur adipisicing.',
       image: '../ShopCard/herba Products Img/herba2.jpg'
     },
     {
-      name: 'Продукт 3',
-      description: 'Опис на продукт 3',
+      name: 'Product 3',
+      description: ' Lorem ipsum dolor, sit amet consectetur adipisicing',
       image: '../ShopCard/herba Products Img/herba6.jpg'
     },
     {
-      name: 'Продукт 4',
-      description: 'Опис на продукт 4',
+      name: 'Product 4',
+      description: ' Lorem ipsum dolor, sit amet consectetur adipisicing',
       image: '../ShopCard/herba Products Img/herba7.jpg'
     },
     {
-      name: 'Продукт 5',
-      description: 'Опис на продукт 5',
+      name: 'Product 5',
+      description: ' Lorem ipsum dolor, sit amet consectetur adipisicing',
       image: '../ShopCard/herba Products Img/herba8.jpg'
     },
     {
-      name: 'Продукт 5',
-      description: 'Опис на продукт 5',
+      name: 'Product 5',
+      description: ' Lorem ipsum dolor, sit amet consectetur adipisicing',
       image: '../ShopCard/herba Products Img/herba44.jpg'
     }
   ];
 
 
-  const productCardsContainer = document.querySelector('.product-cards');
+let productCardsContainer = document.querySelector('.product-cards');
 
   products.forEach((product) => {
-    const cardDiv = document.createElement('div');
+    let cardDiv = document.createElement('div');
     cardDiv.className = 'product-card';
     cardDiv.innerHTML = `
     <div class="card" style="width: 18rem;">
@@ -43,7 +43,7 @@ let products = [
     <div class="card-body">
       <h5 class="card-title">${product.name}e</h5>
       <p class="card-text">${product.description}.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <a href="#" class="btn btn-primary">Add to Card</a>
     </div>
   </div>
     `;
